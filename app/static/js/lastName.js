a060c6357b6651029c0988b079018dc47521846f
Ilsa Gordon <ilsalacious@gmail.com>
2017-03-31 19:01:32 +0000

    JSON serializer was not working, fixed. Flash messages were not working
    Confirming your account and resetting your password were not working, fixed.
    Started adding js, form template and routes for change last name.

---
+++ app/static/js/lastName.js
@@ -0,0 +1,27 @@
+function showCurrentLastName(results) {
+    console.dir(results); // for debugging
+    $(".modal.in").modal("hide");
+    $("#last").load(location.href + " #last");
+}
+
+
+$(document).ready(function() {
+    $('#changeLastName').on('submit', (function(event) {
+        event.preventDefault();
+        var url = $('#changeLastName').attr('action');
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $('#changeLastName').serialize(),
+            success: showCurrentLastName
+        });
+    }));
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", "{{ form.csrf_token._value() }}");
+            }
+        }
+    });
+});
