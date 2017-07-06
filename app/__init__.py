039894b14f76951de6fc96c548d1e29209692e81
Ilsa Gordon <ilsalacious@gmail.com>
2017-07-06 22:49:50 +0000

    Removed local db migrations folder so that it would not be pushed to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -39,7 +39,7 @@ def create_app(config_name):
     app.jinja_env.undefined = StrictUndefined
 
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-        from flask.ext.sslify import SSLify
+        from flask_sslify import SSLify
         sslify = SSLify(app)
 
     from .main import main as main_blueprint
